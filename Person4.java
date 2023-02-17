6740c34d10e28c3263ca691d7e3cb5deebb1e315
Deepyakalavakunta <deepya.kalavakunta@gmail.com>
2023-02-17 13:43:29 +0530

    Added all Person Practices

---
+++ Person4.java
@@ -0,0 +1,11 @@
+class Person4
+{
+static int legs;
+static String food;
+public static void main(String args[])
+{
+Person4 p1=new Person4();
+System.out.println(p1.legs);
+System.out.println(p1.food);
+}
+}
---
+++ Person6.java
@@ -0,0 +1,9 @@
+class Person6
+{
+static int legs;
+public static void main(String args[])
+{
+Person6.legs=2;
+System.out.println(Person6.legs);
+}
+}
