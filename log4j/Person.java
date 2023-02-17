abfccea85115f551c6f5cf76d69bafd0171c4a1a
Deepyakalavakunta <deepya.kalavakunta@gmail.com>
2023-02-17 13:04:55 +0530

    Added Person Practice

---
+++ log4j/Person.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;
+
+public class Person {
+	private static Logger log=Logger.getLogger(Person.class);
+	
+public static void main(String[] args) {
+	log.debug("orange");
+	log.info("Pink");
+	log.error("Yello");
+	log.fatal("Red");
+	
+
+	}
+
+}
