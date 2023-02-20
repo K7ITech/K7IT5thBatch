f1d401705ea3c95adb4aadd1c2686dee1853e889
Veer7899 <panchalveeresh5@gmail.com>
2023-02-21 00:00:30 +0530

    First commit

---
+++ Manager.java
@@ -0,0 +1,24 @@
+
+public class Manager {
+	
+public static void main(String[] args) {
+		
+		Veeresh v1 = new Veeresh();
+		
+		v1.age = 24;
+		v1.name = "veer";
+		v1.height = 6.0;
+		v1.weight = 60.0;
+		v1.color = "Brown";
+		
+		System.out.println(v1.age);
+		System.out.println(v1.name);
+		System.out.println(v1.height);
+		System.out.println(v1.weight);
+		System.out.println(v1.color);
+				
+		
+
+	}
+
+}
---
+++ Veeresh.java
@@ -0,0 +1,38 @@
+
+public class Veeresh {
+	
+	int age;
+	String name;
+	double height;
+	double weight;
+	String color;
+	
+	public double getRunRate()
+	{
+		double runRate = (height*weight)/age;
+		return runRate;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		/*Veeresh v1 = new Veeresh();
+		
+		v1.age = 24;
+		v1.name = "veer";
+		v1.height = 6.0;
+		v1.weight = 60.0;
+		v1.color = "Brown";
+		
+		System.out.println(v1.age);
+		System.out.println(v1.name);
+		System.out.println(v1.height);
+		System.out.println(v1.weight);
+		System.out.println(v1.color);
+				
+		*/
+
+	}
+	
+
+}
