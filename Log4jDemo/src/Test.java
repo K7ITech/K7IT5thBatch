974b36da90eb5520b263a2daca8993e61bb4e9fd
indu <induja.a.v@capgemini.com>
2023-02-21 15:22:29 +0530

    This is Log4jdemo file

---
+++ Log4jDemo/src/Test.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;
+
+public class Test {
+	private static Logger log = Logger.getLogger(Test.class);
+			
+	
+	public static void main(String[] args) {
+		log.debug("indu");
+		log.error("chai");
+		log.fatal("padma");
+		log.info("Ravi");
+
+	}
+
+}
