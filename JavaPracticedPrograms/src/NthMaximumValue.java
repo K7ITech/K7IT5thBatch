d8611e0024fc37095246e5df65b556a4d5b77cfe
Deepya <deepya.kalavakunta@gmail.com>
2023-03-07 12:31:13 +0530

    added JavaPracticedPrograms/.project

---
+++ JavaPracticedPrograms/src/NthMaximumValue.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class NthMaximumValue {
+	
+	private static final Logger log=Logger.getLogger(NthMaximumValue.class);
+
+	public static void main(String[] args) 
+	{
+		
+		
+		int a[]= {10,50,40,30,70};
+		int max=a[0];
+		Arrays.sort(a);
+		int n=3;
+		log.info(a[a.length-n]);
+		
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]>max)
+			{
+				max=a[i];
+			}
+		
+		}
+		log.info("Maximum value of an array:"+max);
+
+	}
+}
+
+
---
+++ JavaPracticedPrograms/src/StringIsPalindromeOrNot.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class StringIsPalindromeOrNot {
+	private static Logger log=Logger.getLogger(StringIsPalindromeOrNot.class);
+	public static void main(String[] args) {
+		
+		
+		/*
+		Scanner s = new Scanner(System.in);
+		log.info("Enter the string");
+		 String inputString=s.next();
+		 String reverseString="";
+		 int n=inputString.length();
+		 for(int i=0;i<n;i++)
+		 {
+			 char c=inputString.charAt(i);
+			 reverseString=c+reverseString;
+		 }
+		 log.info(reverseString);
+		 if(inputString.equals(reverseString))
+		 {
+			 log.info("Given string is Palindrome");
+		 }
+		 else
+		 {
+			 log.info("Given string is not Palindrome");
+		 }
+	*/	
+		int r_num=0;
+		Scanner s = new Scanner(System.in);
+		log.info(" Enter num value");
+		int num=s.nextInt();
+		int org_num=num;
+		while(num>0)
+		{
+			r_num=(r_num*10)+(num%10);
+			num=num/10;
+		}
+		log.info(r_num);
+		
+		if(r_num==org_num)
+		{
+			log.info("Given number is a palindrome");
+		}
+		else
+		{
+			log.info("Given number is not  a palindrome");
+		}
+		
+		
+		
+	}
+
+}
+
+
+
