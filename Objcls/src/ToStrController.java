e8fa700f91b1840e7b8edd1b7eef9e0c7e1e80b1
thulasiramnani <thulasiram1924@gmail.com>
2023-04-04 09:12:17 +0530

    Object class

---
+++ Objcls/src/ToStrController.java
@@ -0,0 +1,9 @@
+
+public class ToStrController {
+public static void main(String[] args) {
+      ToStr t = new ToStr(10,"k7",2.3,33.4);
+      ToStr t1 = new ToStr(11,"k7it",22.3,33.54);
+      System.out.println(t);
+      System.out.println(t1.toString());
+}
+}
