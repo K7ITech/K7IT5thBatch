0f3f2924fe0606fee1f84800e7dda6acf9f1034b
thulasiramnani <thulasiram1924@gmail.com>
2023-04-04 16:46:33 +0530

    Raju file

---
+++ Raju.java
@@ -0,0 +1,13 @@
+public class Raju
+{
+public static void main(String args[]){
+System.out.println("xyz");
+}
+}
+
+class Harish
+{
+public static void main(String args[]){
+System.out.println("adsfwegdrhft");
+}
+}
